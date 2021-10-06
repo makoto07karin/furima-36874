f405c5bcbfe5fc47b69500aeb5139e52bd90c72e
makoto07karin <no.one.best.way07@gmail.com>
2021-10-06 19:02:03 +0900

    手順２の途中commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   #validatesの設定をするが確か
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,} do
+    validates :family_name
+    validates :name
+  end
+  #一度本物がどう動作しているか確認する10/6１つのフォームからを参照
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/,} do
+    validates :family_kana
+    validates :kana
+  end
+
+
   validates :nickname,    presence: true
-  validates :family_name, presence: true
-  validates :name,        presence: true
-  validates :family_kana, presence: true
-  validates :kana,        presence: true
   validates :date,        presence: true
 
   # Association
   has_many :items
   has_many :orders
 end
+
+
+
+#emailとencrypted_passwordに関しては、deviseの機能で入力時の制限がかかっている
+#なので、それ以外は記述する必要がある
