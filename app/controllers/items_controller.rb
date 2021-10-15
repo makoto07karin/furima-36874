5d273f3487c1d4a4b8928ffa43d32b7403cbff7e
makoto07karin <90225685+makoto07karin@users.noreply.github.com>
2021-10-15 12:33:01 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: [:edit, :show]
   before_action :set_item, only: [:show, :update]
   before_action :authenticate_user!, only:[:new, :create]
 
@@ -45,6 +46,13 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end  
 
