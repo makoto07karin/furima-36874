2c26729c9cd6347ecd02c116cdf10b5d9ab1f952
makoto07karin <90225685+makoto07karin@users.noreply.github.com>
2021-10-19 17:20:08 +0900

    手順6 テストコードを実装するOK

--- app/models/order_addresse.rb
+++ app/models/order_addresse.rb
@@ -5,13 +5,13 @@ class OrderAddresse
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code,                    numericality: { with: /\A[0-9]+\z/}
+    validates :post_code,                   format:       {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :city
-    validates :house_number,                numericality: { with: /\A[0-9]+\z/}
-    validates :building_numberty
-    validates :tel,                         numericality: { with: /\A[0-9]+\z/}
+    validates :house_number
+    validates :tel,                         format:       { with: /\A\d{10}\z/,with: /\A\d{11}\z/}
     validates :area_id,                     numericality: { other_than: 0 }
   end
+
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_addresse do
+    post_code{'123-4567'}
+    city{'町'}
+    house_number{'3-3-3'}
+    building_numberty{'ビル'}
+    tel{'02367828888'}
+    area_id{'7'}
+
+  end
+end
