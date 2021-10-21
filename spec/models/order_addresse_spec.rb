f4b94d5efc2abcf793d504ec6d5070c384df739e
makoto07karin <90225685+makoto07karin@users.noreply.github.com>
2021-10-21 22:04:15 +0900

    修正03

--- spec/models/order_addresse_spec.rb
+++ spec/models/order_addresse_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe OrderAddresse, type: :model do
   describe '購入情報の保存' do
     context '正常系' do
       it '全ての値が正しく入力されていれば保存できる' do
+      end 
       
       it 'building_numbertyがなくても保存できる' do
       end 
@@ -54,12 +55,43 @@ RSpec.describe OrderAddresse, type: :model do
         @order_addresse.valid?
         expect(@order_addresse.errors.full_messages).to include("Tel is invalid")
       end
+
+      it 'telが９桁以下では保存できない' do
+        @order_addresse.tel = '123456789'
+        @order_addresse.valid?
+        expect(@order_addresse.errors.full_messages).to include("Tel is invalid")
+      end
+
+      it 'telが12桁以上では保存できない' do
+        @order_addresse.tel ='1234567890123'
+        @order_addresse.valid?
+        expect(@order_addresse.errors.full_messages).to include("Tel is invalid")
+      end
+
+      it 'telが半角数字以外が含まれると保存できない' do
+        @order_addresse.tel = 'asdfghjklo'
+        @order_addresse.valid?
+        expect(@order_addresse.errors.full_messages).to include("Tel is invalid")
+      end
       it "tokenが空では保存できない" do
-        
         @order_addresse.token = nil
         @order_addresse.valid?
         expect(@order_addresse.errors.full_messages).to include("Token can't be blank")
       end
+
+      it 'userが紐付いていなければ購入できない' do
+        @order_addresse.user_id = nil
+        @order_addresse.valid?
+        expect(@order_addresse.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'itemが紐付いていなければ購入できない' do
+        @order_addresse.item_id = nil
+        @order_addresse.valid?
+        expect(@order_addresse.errors.full_messages).to include("Item can't be blank")
+      end
+
+
     end
   end
 end
