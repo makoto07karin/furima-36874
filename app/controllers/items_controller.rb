1eddb55736c154744e8acd968b728471dc06ea43
makoto07karin <90225685+makoto07karin@users.noreply.github.com>
2021-10-25 12:51:19 +0900

    commit02

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :create, :edit]
   before_action :set_item, only: [:show, :update, :edit, :destroy]
-  before_action :move_to_index, only: [:edit, :show, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
   before_action :move_to_index_edit, only: [:edit]
 
 
